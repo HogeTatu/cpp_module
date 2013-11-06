20f6e1c84d2527c4dd1848aeaa8224d46eb9e79d
HogeTatu <hararyo.tae@gmail.com>
2013-11-07 00:20:17 +0900

    update property

--- Property/Property.h
+++ Property/Property.h
@@ -46,7 +46,14 @@ class Property
 {
 public:
 	Property(T& value) : _value(value) {}
+	Property(const Property& ref) : _value(ref._value) {}
 	virtual ~Property() {}
+
+	Property<T>& operator = (const Property<T>& ref)
+	{
+		this->_value = T(ref._value);
+		return *this;
+	}
 
 protected:
 	T&		_value;
@@ -57,14 +64,15 @@ template <typename T, class Getter = SimplePropertyGetter<T>, class Setter = Sim
 class WritableProperty : public Property<T>, private Getter, private Setter
 {
 public:
-	WritableProperty(T& value) : Property(value) {}
+	WritableProperty(T& value) : Property<T>(value) {}
+	WritableProperty(const WritableProperty& ref) : Property<T>(ref) {}
 	virtual ~WritableProperty() {}
 
 public:
-	operator const T& () const { return Get(_value); }
-	const T& operator -> () const { return Get(_value); }
+	operator const T& () const { return this->Get(this->_value); }
+	const T& operator -> () const { return this->Get(this->_value); }
 
-	WritableProperty<T, Getter, Setter>& operator = (const T& var) { Set(_value, var); return *this; }
+	WritableProperty<T, Getter, Setter>& operator = (const T& var) { this->Set(this->_value, var); return *this; }
 };
 
 //! readonly property
@@ -72,12 +80,13 @@ template <typename T, class Getter = SimplePropertyGetter<T>>
 class ReadOnlyProperty : public Property<T>, private Getter
 {
 public:
-	ReadOnlyProperty(T& value) : Property(value) {}
+	ReadOnlyProperty(T& value) : Property<T>(value) {}
+	ReadOnlyProperty(const ReadOnlyProperty& ref) : Property<T>(ref) {}
 	virtual ~ReadOnlyProperty() {}
 
 public:
-	operator const T& () const { return Get(_value); }
-	const T& operator -> () const { return Get(_value); }
+	operator const T& () const { return this->Get(this->_value); }
+	const T& operator -> () const { return this->Get(this->_value); }
 };
 
 }	// end of namespace cpp_module
